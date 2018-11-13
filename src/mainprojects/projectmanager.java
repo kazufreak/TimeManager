16e518c011df9e5a442b48e357348717f920d3d1
kazufreak <kazufeak@gmail.com>
2018-11-13 14:16:42 +0900

    first

---
+++ src/mainprojects/projectmanager.java
@@ -0,0 +1,30 @@
+package mainprojects;
+
+import java.util.Date;
+
+public class projectmanager {
+	String pjname;
+	int pjId;
+	Date dayspan;
+	int MountMoney;
+	String personal;
+	double prosess;
+	String client;
+	String contents;
+	Date claimDate;
+
+	public projectmanager() {
+
+	}
+	private void DBconection() {
+		
+	}
+	public void ProjectRead() {
+		//SQL読込
+	}
+	public void ProjectWrite() {
+		//SQL書込
+	}
+
+
+}
